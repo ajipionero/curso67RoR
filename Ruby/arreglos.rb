8607b5386ea8d8dac3f8936c69b770e56b5aa5e4
Andres Jimenez <andresjimenez.rpo@gmail.com>
2021-07-12 09:05:34 -0400

    arreglos en Ruby

---
+++ Ruby/arreglos.rb
@@ -0,0 +1,18 @@
+a = ["Andres", "Catalina", "Elisha","mascota"]
+b = [37,34,1,0]
+c = []
+#puts a.at(0)
+#puts a.fetch(1)
+#puts b.shuffle
+#puts b.shuffle!
+#puts a.length
+#puts b.join
+#a.insert(-3, 'newbaby')
+#puts a
+#a.delete("mascota")
+puts a
+puts a.values_at(0,1,2).join('-') 
+#b.slice!(3)
+puts b.slice(0)
+puts b.sort
+puts b.reverse 
