71da9193d69d5ec1b2ad72c8697fd95b987371fc
Andres Jimenez <andresjimenez.rpo@gmail.com>
2021-07-12 22:01:32 -0400

    range in ruby

---
+++ Ruby/range.rb
@@ -0,0 +1,20 @@
+=begin
+Desafíos
+Métodos Útiles
+Adelante, intenta los siguientes métodos:
+
+.include?(value) => devuelve true or false
+.last => devuelve el último objeto en el Range
+.max => devuelve el valor máximo en el Range
+.min => devuelve el valor mínimo en el Range
+=end
+
+a= ["Andres","Catalina","Elisha"]
+b= [37,34,1,0]
+puts a.include?("Babygirl") #false
+
+puts a.last
+
+puts b.max
+
+puts b.min
